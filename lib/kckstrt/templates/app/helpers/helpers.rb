76fa842082ede97fe255b4523cc4a6bf5b159c57
Etienne Lemay <etiennelemay21@gmail.com>
2013-01-30 20:43:58 -0500

    Update helpers

--- lib/kckstrt/templates/app/helpers/helpers.rb
+++ lib/kckstrt/templates/app/helpers/helpers.rb
@@ -1,31 +1,33 @@
-# encoding: utf-8
-
 # Assets helpers
-def digest_path(file_name)
-  Assets[file_name].digest_path
+def asset_path(file_name)
+  if ENV['RACK_ENV'] == 'development'
+    file_name = Assets[file_name].logical_path + '?body=1'
+  else
+    file_name = Assets[file_name].digest_path
+  end
+
+  "/assets/#{file_name}"
 end
 
-def asset_path(file_name)
-  asset_host = ENV['ASSET_HOST'] || '/assets/'
-  "#{asset_host}#{digest_path(file_name)}"
+def dependencies_paths(file_name)
+  if ENV['RACK_ENV'] == 'development'
+    Assets[file_name].to_a.map { |dep| dep.logical_path + '?body=1' }
+  else
+    [Assets[file_name].digest_path]
+  end
 end
 
 def javascript_include_tag(file_name)
   file_name = "#{file_name}.js"
-  %(<script src="#{asset_path(file_name)}"></script>)
+  dependencies_paths(file_name).map { |path| %(<script src="/assets/#{path}"></script>) }.join
 end
 
 def stylesheet_link_tag(file_name)
   file_name = "#{file_name}.css"
-  %(<link rel="stylesheet" href="#{asset_path(file_name)}">)
+  dependencies_paths(file_name).map { |path| %(<link rel="stylesheet" href="/assets/#{path}">) }.join
 end
 
-# View helpers
-def partial(partial)
-  partial_view = "partials/_#{partial}".to_sym
-  erb partial_view, :layout => false
-end
-
-def url_for_view(view)
-  view.gsub('_', '-')
+def inline_script_tag(file_name)
+  file_name = "#{file_name}.js"
+  '<script>' + Assets[file_name].source + '</script>'
 end
